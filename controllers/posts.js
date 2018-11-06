9f5a1ba7d73bc09bcf823392795eb6f7e56ccdd3
Edwin Cloud <ecloud412@gmail.com>
2018-11-05 16:33:02 -0800

    :white-check-mark: Added posts-new route and template

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,22 @@
 const router = require('express').Router()
 
-// router.route('/posts')
+router.route('/posts')
+  .get((req, res, next) => {
+
+  })
+  .post((req, res, next) => {
+    
+  })
+  .put((req, res, next) => {
+    
+  })
+  .delete((req, res, next) => {
+    
+  })
+
+router.get('/posts/new', (req, res, next) => {
+  res.render('posts-new')
+})
 
 // Export our router
 module.exports = router
